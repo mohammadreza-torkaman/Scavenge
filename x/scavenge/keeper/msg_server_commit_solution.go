c4fdfbd95898416abbce49045d157e2d7ef526b8
mohammadreza torkaman <torkaman.mohammadreza@yahoo.com>
2022-05-14 17:55:16 +0430

    Commit-solution message implemented

--- x/scavenge/keeper/msg_server_commit_solution.go
+++ x/scavenge/keeper/msg_server_commit_solution.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	"context"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/mohammadreza-torkaman/scavenge/x/scavenge/types"
@@ -10,8 +11,17 @@ import (
 func (k msgServer) CommitSolution(goCtx context.Context, msg *types.MsgCommitSolution) (*types.MsgCommitSolutionResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	_, found := k.GetCommit(ctx, msg.SolutionHash)
+
+	if found {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "This commit already exists")
+	}
+	commit := types.Commit{
+		Index:                 msg.SolutionHash,
+		SolutionHash:          msg.SolutionHash,
+		SolutionScavengerHash: msg.SolutionScavengerHash,
+	}
+	k.SetCommit(ctx, commit)
 
 	return &types.MsgCommitSolutionResponse{}, nil
 }
